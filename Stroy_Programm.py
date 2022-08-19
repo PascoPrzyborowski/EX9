59f1ff21983cb3cf1a6d7a9917db5f8701515d85
PascoPrzyborowski <unity01@gmx.net>
2022-08-19 13:19:34 +0200

    ex9

--- Stroy_Programm.py
+++ Stroy_Programm.py
@@ -13,7 +13,27 @@ if choice == "1":
 elif choice == "2":
    print("u go straight and there is another crossing")
    #continue tree here
+   print("(1) left (2) straight (3) right")
+   elif choice == "1":
+        print("u go left")
+    elif choice == "2":
+        print("u go straight")
+    elif choice == "3":
+        print("u go right")
 elif choice == "3": 
    print("u go right and theres a wall")
 else:
    print("u just die for inaction")
+
+
+
+#    print("u go straight and there is another crossing")
+#    #continue tree here
+#    print("(1) left (2) straight (3) right")
+#    elif choice == "1":
+#         print("u go left")
+#     elif choice == "2":
+#         print("u go straight")
+#     elif choice == "3":
+#         print("u go right")
+
