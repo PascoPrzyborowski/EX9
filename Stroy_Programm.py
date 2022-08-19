f814fa593c87ed0d8355993a21e2ff050159f8fb
PascoPrzyborowski <unity01@gmx.net>
2022-08-19 12:48:26 +0200

    ex9

---
+++ Stroy_Programm.py
@@ -0,0 +1,19 @@
+
+
+
+
+# Adventure Story Program
+
+print("Youre in a dark alley. Where do you go?")
+print("(1) left (2) straight (3) right")
+choice = input("choose: ")
+
+if choice == "1":
+   print("u go left and die")
+elif choice == "2":
+   print("u go straight and there is another crossing")
+   #continue tree here
+elif choice == "3": 
+   print("u go right and theres a wall")
+else:
+   print("u just die for inaction")
